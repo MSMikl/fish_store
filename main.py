c6047361ebfcc8e89d5fe5dd2c842a2c3bbe530f
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-09 15:21:13 +0300

    cart creating and item added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 import requests
 
@@ -7,19 +8,50 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     client_id = os.getenv('CLIENT_ID')
-    BASE_URL = 'https://api.moltin.com'
+    base_url = 'https://api.moltin.com'
     data = {
         'client_id': client_id,
         'grant_type': 'implicit'
     }
-    response = requests.post(f'{BASE_URL}/oauth/access_token', data=data)
+    response = requests.post(f'{base_url}/oauth/access_token', data=data)
     response.raise_for_status()
     store_token = f"Bearer {response.json().get('access_token')}"
     headers = {
         'Authorization': store_token
     }
-    response = requests.get(f'{BASE_URL}/v2/products', headers=headers)
-    print(response.json())
+    response = requests.get(f'{base_url}/v2/products', headers=headers)
+    response.raise_for_status()
+    
+    cart_data = {
+        'data':{
+            'name': 'test_cart'
+        }
+    }
+    
+    response = requests.post(f'{base_url}/v2/carts', headers=headers, json=cart_data)
+    response.raise_for_status()
+    cart = response.json()['data']['id']
+    add_item_to_cart(store_token, base_url, cart, '10001', 2)
+    
+def add_item_to_cart(token, url, cart_id, sku, quantity):
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'data': {
+            'sku': sku,
+            'quantity': quantity,
+            "type": "cart_item"                        
+        }
+    }
+    response = requests.post(
+        f'{url}/v2/carts/{cart_id}/items',
+        headers=headers,
+        json=data
+    )
+    pprint.pprint(response.json())
+    
     
     
 if __name__ == '__main__':
