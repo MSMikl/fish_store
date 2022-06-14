145c30d98e6bcebc5f2a48d2c99308095f49cdc5
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-14 12:57:37 +0300

    Create tg_bot.py

---
+++ tg_bot.py
@@ -0,0 +1,83 @@
+import os
+import redis
+
+from dotenv import load_dotenv
+
+from telegram.ext import Filters, Updater, CallbackContext
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+
+DB = None
+
+
+def start(update: Updater, context: CallbackContext):
+    update.message.reply_text(text='Приветствуем в рыбном магазине')
+    return 'ECHO'
+
+
+def echo(update: Updater):
+    update.message.reply_text(text=update.message.text)
+    return 'ECHO'
+
+
+def get_db_connection():
+    global DB
+    if not DB:
+        db_pass = os.getenv('DB_PASS')
+        db_host = os.getenv('DB_HOST')
+        db_port = int(os.getenv('DB_PORT'))
+        DB = redis.Redis(
+            host=db_host,
+            port=db_port,
+            password=db_pass,
+            db=1
+        )
+    return DB
+
+
+def user_input_handler(update: Updater, context: CallbackContext):
+    db = get_db_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query.data:
+        user_reply = update.callback_query.data
+        chat_id = update.callbackquery.message.chat_id
+    else:
+        return
+
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode('UTF-8')
+
+    states_function = {
+        'START': start,
+        'ECHO': echo
+    }
+
+    state_handler = states_function[user_state]
+    
+    next_state = state_handler(update, context)
+    db.set(chat_id, next_state)
+
+
+def main():
+    load_dotenv()
+    tg_token = os.getenv('TG_TOKEN')
+    updater = Updater(tg_token)
+
+    bot_commands = [
+        ('start', 'Начать диалог')
+    ]
+    updater.bot.set_my_commands(bot_commands)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(user_input_handler))
+    dispatcher.add_handler(MessageHandler(Filters.text, user_input_handler))
+    dispatcher.add_handler(CommandHandler('start', user_input_handler))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
