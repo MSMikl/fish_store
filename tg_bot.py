91453ecfba12722025267f855bf0cbd7ebf46c9d
MSMikl <michalbl4@gmail.com>
2022-06-23 09:58:25 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -3,6 +3,7 @@ import redis
 
 from dotenv import load_dotenv
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Filters, Updater, CallbackContext
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 
@@ -10,12 +11,33 @@ from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 DB = None
 
 
-def start(update: Updater, context: CallbackContext):
-    update.message.reply_text(text='Приветствуем в рыбном магазине')
+def start(update: Update, context: CallbackContext):
+    keyboard = [
+        [
+            InlineKeyboardButton('Option 1', callback_data=1),
+            InlineKeyboardButton('Option 2', callback_data=2)
+        ],
+        [InlineKeyboardButton('Option 3', callback_data=3)]
+    ]
+
+    update.message.reply_text(
+        text='Приветствуем в рыбном магазине. Выберите опцию:',
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
     return 'ECHO'
 
 
-def echo(update: Updater, context: CallbackContext):
+def button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    context.bot.edit_message_text(
+        text=f"Вы выбрали вариант {query.data}",
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id
+
+    )
+    return 'ECHO'
+
+def echo(update: Update, context: CallbackContext):
     update.message.reply_text(text=update.message.text)
     return 'ECHO'
 
@@ -35,14 +57,14 @@ def get_db_connection():
     return DB
 
 
-def user_input_handler(update: Updater, context: CallbackContext):
+def user_input_handler(update: Update, context: CallbackContext):
     db = get_db_connection()
     if update.message:
         user_reply = update.message.text
         chat_id = update.message.chat_id
     elif update.callback_query.data:
         user_reply = update.callback_query.data
-        chat_id = update.callbackquery.message.chat_id
+        chat_id = update.callback_query.message.chat_id
     else:
         return
 
@@ -53,7 +75,7 @@ def user_input_handler(update: Updater, context: CallbackContext):
 
     states_function = {
         'START': start,
-        'ECHO': echo
+        'ECHO': button
     }
 
     state_handler = states_function[user_state]
